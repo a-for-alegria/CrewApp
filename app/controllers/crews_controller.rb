8365949690dfa2f1e3dcf3853e79a0f165813054
a-for-alegria <alegria2112@gmail.com>
2014-12-04 20:12:09 +0200

    add create action

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -1,7 +1,24 @@
 class CrewsController < ApplicationController
 	def index
+		@crews = Crew.all
 	end
 
 	def new
+		@crew = Crew.new
 	end
+
+	def create
+    @crew = Crew.new(crew_params)
+    if @crew.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def crew_params
+      params.require(:crew).permit(:name, :secondname)
+    end
 end
--- spec/requests/crew_pages_spec.rb
+++ spec/requests/crew_pages_spec.rb
@@ -16,4 +16,26 @@ describe "CrewPage" do
     it {should have_content('New member')}
     it {should have_link('To Dashboard', href: root_path)}
   end
+
+  describe "New crew member page" do
+    before {visit new_crew_path}
+
+    let(:submit) {"Create"}
+
+    describe "Invalid creation" do
+      it "should not create crew member" do
+        expect{click_button submit}.not_to change(Crew, :count)
+      end
+    end
+
+    describe "Valid crew member creation" do
+      before do
+        fill_in "Name",               with: "Ace"
+        fill_in "Secondname",         with: "Team"
+      end
+      it "should create crew member" do
+        expect{click_button submit}.to change(Crew, :count).by(1)
+      end
+    end
+  end
 end
