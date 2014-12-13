cd93faf8df5b58a929b15f7eb8cc7cae8d9494a7
a-for-alegria <alegria2112@gmail.com>
2014-12-13 16:29:55 +0200

    add styles to new/edit pages

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,6 +1,6 @@
 class Crew < ActiveRecord::Base
   validates :name, :secondname, presence: true
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :avatar
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def combine_names
