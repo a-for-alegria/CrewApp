19042413a13b45d60d592789c6778aae956d5e4d
a-for-alegria <alegria2112@gmail.com>
2015-03-04 12:00:52 +0200

    add first jquery function for close errors on click

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  
+  $('.alert').on('click', function(){
+  	$('.alert').remove();
+  })
+
+  $('.success').on('click', function(){
+  	$('.success').remove();
+  })
+});
