2454ccd00a4267d014355b5f7fb0e3b87f096237
a-for-alegria <alegria2112@gmail.com>
2015-04-01 16:16:39 +0300

    save current version before change client to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 CrewApp::Application.routes.draw do
   devise_for :users,controllers: {sessions: "user/sessions"}
-  get "start_pages/start"
 
   resources :crews
   resources :clients
 
-	root 'crews#index'
+  root 'crews#index'
 end
--- spec/requests/client_pages_spec.rb
+++ spec/requests/client_pages_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe 'ClientPage' do
   subject {page}
   let(:user) {FactoryGirl.create(:user)}
-  let(:client) {Client.create(client_name: client_name, client_secondname: client_secondname, budget: budget)}
+  let(:client) {user.clients.build(client_name: client_name, client_secondname: client_secondname, 
+      budget: budget, project: project)}
   let(:client_name) {'Client'}
   let(:client_secondname) {'First'}
   let(:budget) {3000}
+  let(:project) {'Project'}
 
   before {sign_in(user)}
   #==============================New page===========>>
@@ -31,6 +33,7 @@ describe 'ClientPage' do
           fill_in 'Client name',               with: client_name
           fill_in 'Client secondname',         with: client_secondname
           fill_in 'Budget',                    with: budget
+          fill_in 'Project',                   with: project
         end
         it 'should create client' do
           expect{click_button submit}.to change(Client, :count).by(1)
@@ -52,6 +55,7 @@ describe 'ClientPage' do
     let(:reload_cn) {client_name*2}
     let(:reload_csn) {client_secondname*2}
     let(:reload_bgt) {client.budget*2}
+    let(:reload_prg) {client.project*2}
 
     before {visit edit_client_path(client)}
     
@@ -62,11 +66,13 @@ describe 'ClientPage' do
         fill_in 'Client name',              with: reload_cn
         fill_in 'Client secondname',        with: reload_csn
         fill_in 'Budget',                   with: reload_bgt
+        fill_in 'Project',                  with: reload_prg
         click_button('Save')
       end
       it {should have_content(reload_cn)}
       it {should have_content(reload_csn)}
       it {should have_content(reload_bgt)}
+      it {should have_content(reload_prg)}
     end
   end
 
@@ -74,7 +80,7 @@ describe 'ClientPage' do
 
 	describe 'delete' do
 		before do
-			@client = Client.create(client_name: client_name, client_secondname: client_secondname, budget: budget)
+			@client = user.clients.build(client_name: client_name, client_secondname: client_secondname, budget: budget, project: project)
 			visit root_path
 		end
 
